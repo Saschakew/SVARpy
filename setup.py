ed5e9ae98b403768eb2a3b9c0303c0689bc91d5f
Sascha Keweloh <sascha.keweloh@tu-dortmund.de>
2020-11-23 13:22:40 +0100

    setup.py und cmds

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ with open('README.md') as f:
 
 with open('LICENSE') as f:
     license = f.read()
+ 
 
 setuptools.setup(
     name="SVARpy", # Replace with your own username
-    version="0.1.0",
+    version="0.1.6",
     author="Sascha Keweloh",
     author_email="sascha.keweloh@tu-dortmund.de",
     description="SVAR estimation",
     long_description=readme,
     url="https://github.com/Saschakew/SVARpy",
     packages=setuptools.find_packages(),
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
