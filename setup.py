f43dee01ba9cff1c676f26eb099ed86a6387b8a5
Sascha Keweloh <sascha.keweloh@tu-dortmund.de>
2024-07-16 09:33:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('LICENSE') as f:
 
 setuptools.setup(
     name="SVARpy",
-    version="0.1.7",
+    version="0.1.8",
     author="Sascha Keweloh",
     author_email="sascha.keweloh@tu-dortmund.de",
     description="SVAR estimation",
