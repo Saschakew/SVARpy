d7ef256919f0820657b43a1fe79211f44593fb88
Sascha Keweloh <sascha.keweloh@tu-dortmund.de>
2025-07-09 18:07:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except FileNotFoundError:
 
 setuptools.setup(
   name="SVARpy",
-  version="0.1.15",
+  version="0.1.16",
   author="Sascha Keweloh",
   author_email="sascha.keweloh@tu-dortmund.de",
   description="SVAR estimation",
